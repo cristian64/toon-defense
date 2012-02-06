3ba1e8ba62f11cee1b2039b5a17316a3ce84ecc2
cristian64@gmail.com <cristian64@gmail.com@72a01fba-5eab-6e83-61fc-fb7fde2ef1ea>
2012-02-06 22:29:24 +0000

    Added a ParticleSystem to generate explosions, smoke, and other things.

--- ToonDefense/ToonDefense/ToonDefense/GameplayComponent.cs
+++ ToonDefense/ToonDefense/ToonDefense/GameplayComponent.cs
@@ -10,19 +10,28 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using ToonDefense.Spaceships;
+using ToonDefense.ParticleSystem;
 
 namespace ToonDefense
 {
     public class GameplayComponent : DrawableGameComponent
     {
         List<DrawableGameComponent> drawableComponents;
+        List<DrawableGameComponent> particleSystems;
         List<GameComponent> components;
         Camera camera;
+
+        FireParticleSystem fireParticleSystem;
+        ExplosionParticleSystem explosionParticleSystem;
+        ExplosionSmokeParticleSystem explosionSmokeParticleSystem;
+        ProjectileTrailParticleSystem projectileTrailParticleSystem;
+        SmokePlumeParticleSystem smokePlumeParticleSystem;
 
         public GameplayComponent(Game game)
             : base(game)
         {
             drawableComponents = new List<DrawableGameComponent>();
+            particleSystems = new List<DrawableGameComponent>();
             components = new List<GameComponent>();
         }
 
@@ -35,6 +44,18 @@ namespace ToonDefense
 
             World world = new World(Game, camera);
             drawableComponents.Add(world);
+
+            fireParticleSystem = new FireParticleSystem(Game, Game.Content, camera);
+            explosionParticleSystem = new ExplosionParticleSystem(Game, Game.Content, camera);
+            explosionSmokeParticleSystem = new ExplosionSmokeParticleSystem(Game, Game.Content, camera);
+            projectileTrailParticleSystem = new ProjectileTrailParticleSystem(Game, Game.Content, camera);
+            smokePlumeParticleSystem = new SmokePlumeParticleSystem(Game, Game.Content, camera);
+
+            particleSystems.Add(fireParticleSystem);
+            particleSystems.Add(explosionParticleSystem);
+            particleSystems.Add(explosionSmokeParticleSystem);
+            particleSystems.Add(projectileTrailParticleSystem);
+            particleSystems.Add(smokePlumeParticleSystem);
 
             Interceptor interceptor = new Interceptor(Game, camera);
             interceptor.Position = new Vector3(3, 1, -4);
@@ -54,6 +75,8 @@ namespace ToonDefense
 
             foreach (DrawableGameComponent i in drawableComponents)
                 i.Initialize();
+            foreach (DrawableGameComponent i in particleSystems)
+                i.Initialize();
             foreach (GameComponent i in components)
                 i.Initialize();
             base.Initialize();
@@ -61,7 +84,20 @@ namespace ToonDefense
 
         public override void Update(GameTime gameTime)
         {
+            if (Keyboard.GetState().IsKeyDown(Keys.D1))
+                explosionParticleSystem.AddParticle(new Vector3(0, 1, 0), new Vector3(0, 0, 0));
+            if (Keyboard.GetState().IsKeyDown(Keys.D2))
+                projectileTrailParticleSystem.AddParticle(new Vector3(1, 1, 0), new Vector3(0, 0, 0));
+            if (Keyboard.GetState().IsKeyDown(Keys.D3))
+                fireParticleSystem.AddParticle(new Vector3(1, 2, 0), new Vector3(0, 0, 0));
+            if (Keyboard.GetState().IsKeyDown(Keys.D4))
+                smokePlumeParticleSystem.AddParticle(new Vector3(-1, 2, 0), new Vector3(0, 0, 0));
+            if (Keyboard.GetState().IsKeyDown(Keys.D5))
+                explosionSmokeParticleSystem.AddParticle(new Vector3(-1, 1, 0), new Vector3(0, 0, 0));
+
             foreach (DrawableGameComponent i in drawableComponents)
+                i.Update(gameTime);
+            foreach (DrawableGameComponent i in particleSystems)
                 i.Update(gameTime);
             foreach (GameComponent i in components)
                 i.Update(gameTime);
@@ -76,6 +112,8 @@ namespace ToonDefense
             GraphicsDevice.Clear(Color.RosyBrown);
 
             foreach (DrawableGameComponent i in drawableComponents)
+                i.Draw(gameTime);
+            foreach (DrawableGameComponent i in particleSystems)
                 i.Draw(gameTime);
             base.Draw(gameTime);
         }
