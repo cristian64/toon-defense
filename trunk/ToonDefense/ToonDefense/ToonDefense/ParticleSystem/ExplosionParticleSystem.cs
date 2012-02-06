a765329fa74e3f5808460c051c35d1ae6514122e
cristian64@gmail.com <cristian64@gmail.com@72a01fba-5eab-6e83-61fc-fb7fde2ef1ea>
2012-02-06 22:29:24 +0000

    Added a ParticleSystem to generate explosions, smoke, and other things.

---
+++ trunk/ToonDefense/ToonDefense/ToonDefense/ParticleSystem/ExplosionParticleSystem.cs
@@ -0,0 +1,62 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// ExplosionParticleSystem.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace ToonDefense.ParticleSystem
+{
+    /// <summary>
+    /// Custom particle system for creating the fiery part of the explosions.
+    /// </summary>
+    class ExplosionParticleSystem : ParticleSystem
+    {
+        public ExplosionParticleSystem(Game game, ContentManager content, Camera camera)
+            : base(game, content, camera)
+        { }
+
+
+        protected override void InitializeSettings(ParticleSettings settings)
+        {
+            settings.TextureName = "explosion";
+
+            settings.MaxParticles = 100;
+
+            settings.Duration = TimeSpan.FromSeconds(2);
+            settings.DurationRandomness = 1;
+
+            settings.MinHorizontalVelocity = 0.01f;
+            settings.MaxHorizontalVelocity = 0.05f;
+
+            settings.MinVerticalVelocity = -0.1f;
+            settings.MaxVerticalVelocity = 0.1f;
+
+            settings.EndVelocity = 0;
+
+            settings.MinColor = Color.DarkGray;
+            settings.MaxColor = Color.Gray;
+
+            settings.MinRotateSpeed = -1;
+            settings.MaxRotateSpeed = 1;
+
+            settings.MinStartSize = 0.5f;
+            settings.MaxStartSize = 0.5f;
+
+            settings.MinEndSize = 1.5f;
+            settings.MaxEndSize = 2.0f;
+
+            // Use additive blending.
+            settings.BlendState = BlendState.Additive;
+        }
+    }
+}
---
+++ trunk/ToonDefense/ToonDefense/ToonDefense/ParticleSystem/ProjectileTrailParticleSystem.cs
@@ -0,0 +1,60 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// ProjectileTrailParticleSystem.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace ToonDefense.ParticleSystem
+{
+    /// <summary>
+    /// Custom particle system for leaving smoke trails behind the rocket projectiles.
+    /// </summary>
+    class ProjectileTrailParticleSystem : ParticleSystem
+    {
+        public ProjectileTrailParticleSystem(Game game, ContentManager content, Camera camera)
+            : base(game, content, camera)
+        { }
+
+
+        protected override void InitializeSettings(ParticleSettings settings)
+        {
+            settings.TextureName = "smoke";
+
+            settings.MaxParticles = 1000;
+
+            settings.Duration = TimeSpan.FromSeconds(3);
+
+            settings.DurationRandomness = 1.5f;
+
+            settings.EmitterVelocitySensitivity = 0.1f;
+
+            settings.MinHorizontalVelocity = -0.1f;
+            settings.MaxHorizontalVelocity = 0.1f;
+
+            settings.MinVerticalVelocity = -0.1f;
+            settings.MaxVerticalVelocity = 0.1f;
+
+            settings.MinColor = new Color(64, 96, 128, 255);
+            settings.MaxColor = new Color(255, 255, 255, 128);
+
+            settings.MinRotateSpeed = -4;
+            settings.MaxRotateSpeed = 4;
+
+            settings.MinStartSize = 0.2f;
+            settings.MaxStartSize = 0.4f;
+
+            settings.MinEndSize = 0.3f;
+            settings.MaxEndSize = 0.5f;
+        }
+    }
+}
