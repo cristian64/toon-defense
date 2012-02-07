208d1cbdad012506b01377ae29f0d2643131c266
cristian64@gmail.com <cristian64@gmail.com@72a01fba-5eab-6e83-61fc-fb7fde2ef1ea>
2012-02-07 19:45:59 +0000

    Now the spaceships has a target to move to. The angle is set up accordingly.

--- ToonDefense/ToonDefense/ToonDefense/Spaceships/Spaceship.cs
+++ ToonDefense/ToonDefense/ToonDefense/Spaceships/Spaceship.cs
@@ -14,7 +14,7 @@ namespace ToonDefense.Spaceships
 {
     public class Spaceship : Object
     {
-        public List<Vector3> Destinations;
+        public List<Vector2> Destinations;
         public float Speed;
         public int Health;
         public int Reward;
@@ -22,7 +22,35 @@ namespace ToonDefense.Spaceships
         public Spaceship(Game game, Camera camera)
             :base(game, camera)
         {
+            Destinations = new List<Vector2>();
+            Speed = 1;
+            Health = 100;
+            Reward = 100;
+        }
 
+        public override void Update(GameTime gameTime)
+        {
+            if (Destinations.Count > 0 && gameTime.ElapsedGameTime.TotalMilliseconds > 0)
+            {
+                Vector2 position = new Vector2(Position.X, Position.Z);
+                Vector2 destination = Destinations[0];
+                Vector2 direction = destination - position;
+
+                if (direction.LengthSquared() > Speed * Speed / gameTime.ElapsedGameTime.TotalMilliseconds / gameTime.ElapsedGameTime.TotalMilliseconds)
+                {
+                    direction.Normalize();
+                    Position.X += direction.X * Speed / (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+                    Position.Z += direction.Y * Speed / (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+                    Rotation.Y = (float)Math.Atan2(-direction.Y, direction.X);
+                }
+                else
+                {
+                    Position.X = destination.X;
+                    Position.Z = destination.Y;
+                    Destinations.RemoveAt(0);
+                }
+            }
+            base.Update(gameTime);
         }
     }
 }
